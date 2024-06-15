da0acd5c1a0f1743cca0b5e16bd108325e585cb9
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-15 16:21:41 +0700

    Test Api BE

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -4,13 +4,22 @@ import AppBar from '~/components/AppBar/AppBar';
 import BoadBar from './BoardBar/BoardBar';
 import BoardContent from './BoardContent/BoardContent';
 import { mockData } from '~/apis/mock-data';
+import { useEffect, useState } from 'react';
+import { fetchBoardDetailsAPI } from '~/apis';
 
 function Board() {
+    const [board, setBoard] = useState(null);
+
+    useEffect(() => {
+        const boardId = '666d4de2ebcd8206e719d8d0';
+        fetchBoardDetailsAPI(boardId).then((board) => setBoard(board));
+    }, []);
+
     return (
         <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
             <AppBar />
-            <BoadBar board={mockData?.board} />
-            <BoardContent board={mockData?.board} />
+            <BoadBar board={board} />
+            <BoardContent board={board} />
         </Container>
     );
 }
