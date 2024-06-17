543f0b3e784069250c9df112e4091b7fe1ffa22b
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-17 14:10:54 +0700

    TEST

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,9 @@ if (process.env.BUILD_MODE === 'dev') {
     apiRoot = 'http://localhost:8017';
 }
 if (process.env.BUILD_MODE === 'production') {
-    apiRoot = 'https://trello-api-mqh6.onrender.com/';
+    apiRoot = 'https://trello-backend-yf5y.onrender.com';
 }
-console.log('🚀 ~ apiRoot:', apiRoot);
 export const API_ROOT = apiRoot;
+
+// export const API_ROOT = 'http://localhost:8017';
+// export const API_ROOT = 'https://trello-backend-yf5y.onrender.com';
