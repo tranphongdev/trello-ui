88a6e2bfa01a5c18edff0f4af7a0120d6f19e0b5
dinhphong02 <tdp.dinhphong@gmail.com>
2024-06-17 12:22:33 +0700

    Fix build mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,9 @@
-export const API_ROOT = 'http://localhost:8017';
+let apiRoot = '';
+if (process.env.BUILD_MODE === 'dev') {
+    apiRoot = 'http://localhost:8017';
+}
+if (process.env.BUILD_MODE === 'production') {
+    apiRoot = 'https://trello-api-mqh6.onrender.com/';
+}
+console.log('🚀 ~ apiRoot:', apiRoot);
+export const API_ROOT = apiRoot;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    define: {
+        'process.env': process.env,
+    },
     plugins: [react()],
     resolve: {
         alias: [{ find: '~', replacement: '/src' }],
